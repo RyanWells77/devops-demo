be9040097492364165159b9ab99a7442332f0c70
Ryan Wells <rswells2@gmail.com>
2024-02-29 11:28:18 -0700

    ready to deploy

---
+++ public/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '49b93aae0d544b14af14a87a4c52a439',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.use(express.json())
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    rollbar.info("someone got the student list")
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.warning("someone tried to add an empty string")
+           res.status(400).send('You must enter a name.')
+       } else {
+            // rollbar.error("someone tried to duplicate a student")
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    // rollbar.critical("Someone deleted a student!")
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
